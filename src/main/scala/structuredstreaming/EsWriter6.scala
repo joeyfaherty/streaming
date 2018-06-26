e09dafe87d3ee22dcb9c52eb31a59c8e2c84200a
joeyfaherty <joeyfaherty@live.ie>
2018-06-26 18:59:51 +0200

    Example classes to write to ES v5 and also ES v6

---
+++ src/main/scala/structuredstreaming/EsWriter6.scala
@@ -0,0 +1,62 @@
+package structuredstreaming
+
+import java.util.concurrent.TimeUnit
+
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+import org.apache.spark.sql.{Dataset, SparkSession}
+import structuredstreaming.SimpleStructuredSreaming.{Customer, getClass}
+
+class EsWriter6 {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      // run app locally utilizing all cores
+      .master("local[*]")
+      .appName(getClass.getName)
+      .config("es.nodes", "localhost")
+      .config("es.port", "9200")
+      .config("es.index.auto.create", "true") // this will ensure that index is also created on first POST
+      .config("es.nodes.wan.only", "true") // needed to run against dockerized ES for local tests
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val customerEvents: Dataset[Customer] = spark
+      .readStream
+      .format("kafka")
+      // maximum number of offsets processed per trigger interval
+      .option("maxOffsetsPerTrigger", 100)
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "customer-topic")
+      .option("startingOffsets", "earliest")
+      .option("failOnDataLoss", "false")
+      .load()
+      .select(
+        $"key" cast "string", // deserialize binary key
+        $"value" cast "string", // deserialize binary value
+        $"topic",
+        $"partition",
+        $"offset"
+      )
+      .as[(String, String, String, String, String)]
+      // convert kafka value to case class
+      .map(x => {
+      val split = x._2.split(",")
+      val age = split(3).toInt
+      Customer(split(0), split(1), split(2), age, age > 18, System.currentTimeMillis())
+    })
+
+    customerEvents
+      .writeStream
+      .outputMode(OutputMode.Append())
+      .format("org.elasticsearch.spark.sql")
+      .option("checkpointLocation", "/checkpointLocation")
+      .option("es.mapping.id", "mappingColumn")
+      .trigger(Trigger.ProcessingTime(5, TimeUnit.SECONDS))
+      .start("index/type")
+      .awaitTermination()
+
+  }
+
+}
